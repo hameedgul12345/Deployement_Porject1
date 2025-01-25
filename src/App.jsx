09832d93c6e60af55465cb8638f87397d552e919
Hameed Gul <hamidkhan31590@gmail.com>
2025-01-25 20:22:07 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './compoents/Home'
-import About from './compoents/About'
+import Home from './components/Home'
+import About from './components/About'
 function App() {
  
 
