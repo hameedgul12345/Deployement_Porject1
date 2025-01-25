d4dd50a618abead13adfc68679d3bf18b0a7d89b
Hameed Gul <hamidkhan31590@gmail.com>
2025-01-25 20:15:25 +0500

    Resolved merge conflicts and updated imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './compoents/Home'
+import Home from './components/Home'
 import About from './components/About';
 import Services from './components/Services';
 
