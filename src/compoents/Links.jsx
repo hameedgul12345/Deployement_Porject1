328d3549d963a1212a328b1bdd8d1ea538caf0a0
Hameed Gul <hamidkhan31590@gmail.com>
2025-01-25 19:45:51 +0500

    services

--- src/compoents/Links.jsx
+++ src/compoents/Links.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Links() {
   return (
-   <>
-   <Link to={'/'}>Home</Link>
-   <Link to={'/services'}>Services</Link>
-   
-   </>
-  )
+    <>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About</Link>
+      <Link to={"/services"}>Services</Link>
+    </>
+  );
 }
 
-export default Links
+export default Links;
